856fe938ec68fecbd631ff1163c6b307d8b879f9
“Florence <162617498+TheTable1@users.noreply.github.com>
2025-02-10 23:18:22 +0700

    update table over all

--- code/Project_2-master/resources/views/logs/logs-over-all.blade.php
+++ code/Project_2-master/resources/views/logs/logs-over-all.blade.php
@@ -10,7 +10,7 @@
     <table class="min-w-full table-auto shadow-md rounded-lg overflow-hidden">
         <thead style="background-color:#000000;" class="text-white">
             <tr>
-                <th class="py-3 px-4 text-left">No</th>
+                <th style="border: 1px solid #000000;" class="py-3 px-4 text-left">No</th>
                 <th class="py-3 px-4 text-left">User</th>
                 <th class="py-3 px-4 text-left">Action</th>
                 <th class="py-3 px-4 text-left">Description</th>
@@ -23,18 +23,18 @@
         <tbody>
             @foreach ($logs as $log)
             <tr class="border-b hover:bg-gray-50">
-                <td class="py-3 px-4">{{ ($logs->currentPage() - 1) * $logs->perPage() + $loop->iteration }}</td> <!-- คำนวณหมายเลขแถว -->
-                <td class="py-3 px-4">{{ $log->user->email ?? 'N/A' }}</td>
-                <td class="py-3 px-4">{{ $log->action }}</td>
-                <td class="py-3 px-4">{{ $log->description }}</td>
-                <td class="py-3 px-4">{{ $log->ip_address }}</td>
-                <td class="py-3 px-4">{{ $log->created_at->format('Y-m-d H:i:s') }}</td>
+                <td style="border: 1px solid rgb(185, 185, 185);" class="py-3 px-4">{{ ($logs->currentPage() - 1) * $logs->perPage() + $loop->iteration }}</td> <!-- คำนวณหมายเลขแถว -->
+                <td style="border: 1px solid rgb(185, 185, 185)" class="py-3 px-4">{{ $log->user->email ?? 'N/A' }}</td>
+                <td style="border: 1px solid rgb(185, 185, 185)" class="py-3 px-4">{{ $log->action }}</td>
+                <td style="border: 1px solid rgb(185, 185, 185)" class="py-3 px-4">{{ $log->description }}</td>
+                <td style="border: 1px solid rgb(185, 185, 185)" class="py-3 px-4">{{ $log->ip_address }}</td>
+                <td style="border: 1px solid rgb(185, 185, 185)" class="py-3 px-4">{{ $log->created_at->format('Y-m-d H:i:s') }}</td>
             </tr>
             @endforeach
         </tbody>
     </table>
 
-    <div class="mt-4">
+    <div class="mt-4 text-black">
         {{ $logs->links() }} <!-- สำหรับ pagination -->
     </div>
 </div>
