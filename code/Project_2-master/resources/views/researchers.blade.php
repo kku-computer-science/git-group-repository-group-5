e90350f7d5c5988f068a2af81f1220a10f438742
“Florence <162617498+TheTable1@users.noreply.github.com>
2025-02-26 03:30:21 +0700

    Florence Ferrara 653380210-1
    ทำงานผิด branch

--- code/Project_2-master/resources/views/researchers.blade.php
+++ code/Project_2-master/resources/views/researchers.blade.php
@@ -1,17 +1,24 @@
 @extends('layouts.layout')
 @section('content')
 <div class="container card-2">
-    <p class="title"> Researchers </p>
+    <p class="title">{{ trans('books.Researchers') }} </p>
     @foreach($request as $res)
     <span>
-        <ion-icon name="caret-forward-outline" size="small"></ion-icon> {{$res->program_name_en}}
+        <ion-icon name="caret-forward-outline" size="small"></ion-icon>
+        @if (app()->getLocale() == 'th')
+        {{ $res->program_name_th }}
+        @elseif (app()->getLocale() == 'zh')
+        {{$res->program_name_cn }}
+        @else
+        {{$res->program_name_en }}
+        @endif
     </span>
     <div class="d-flex">
         <div class="ml-auto">
             <form class="row row-cols-lg-auto g-3" method="GET" action="{{ route('searchresearchers',['id'=>$res->id])}}">
                 <div class="col-md-8">
                     <div class="input-group">
-                        <input type="text" class="form-control" name="textsearch" placeholder="Research interests">
+                        <input type="text" class="form-control" name="textsearch" placeholder="{{ trans('books.Researchinterests') }}">
                     </div>
                 </div>
                 <!-- <div class="col-12">
@@ -24,7 +31,7 @@
                         </select>
                     </div> -->
                 <div class="col-md-4">
-                    <button type="submit" class="btn btn-outline-primary">Search</button>
+                    <button type="submit" class="btn btn-outline-primary">{{ trans('books.Search') }}</button>
                 </div>
             </form>
         </div>
@@ -39,32 +46,38 @@
                     <div class="col-sm-4">
                         <img class="card-image" src="{{ $r->picture}}" alt="">
                     </div>
-                    <div class="col-sm-8 overflow-hidden" style="text-overflow: clip; @if(app()->getLocale() == 'en') max-height: 220px; @else max-height: 210px;@endif">
+                    <div class="col-sm-8 overflow-hidden" style="text-overflow: clip; @if(app()->getLocale() == 'en') max-height: 220px; @elseif(app()->getLocale() == 'zh') max-height: 215px; @else max-height: 210px;@endif">
                         <div class="card-body">
                             @if(app()->getLocale() == 'en')
+                            @if($r->doctoral_degree == 'Ph.D.')
+                            <h5 class="card-title">{{ $r->{'fname_en'} }} {{ $r->{'lname_en'} }}, {{$r->doctoral_degree}}</h5>
+                            @else
+                            <h5 class="card-title">{{ $r->{'fname_en'} }} {{ $r->{'lname_en'} }}</h5>
+                            @endif
+                            <h5 class="card-title">{{ $r->{'academic_ranks_en'} }}</h5>
+                            @elseif(app()->getLocale() == 'zh')
+                            @if($r->doctoral_degree == 'Ph.D.')
+                            <h5 class="card-title">{{ $r->{'fname_en'} }} {{ $r->{'lname_en'} }}, {{$r->doctoral_degree}}</h5>
+                            @else
+                            <h5 class="card-title">{{ $r->{'fname_en'} }} {{ $r->{'lname_en'} }}</h5>
+                            @endif
+                            <h5 class="card-title">{{ $r->{'academic_ranks_cn'} }}</h5>
+                            @else
+                            <h5 class="card-title">{{ $r->{'position_'.app()->getLocale()} }}
+                                {{ $r->{'fname_'.app()->getLocale()} }} {{ $r->{'lname_'.app()->getLocale()} }}
+                            </h5>
+                            <h5 class="card-title">{{ $r->{'academic_ranks_th'} }}</h5>
+                            @endif
 
-                                @if($r->doctoral_degree == 'Ph.D.')
-                                <h5 class="card-title">{{ $r->{'fname_'.app()->getLocale()} }} {{ $r->{'lname_'.app()->getLocale()} }}, {{$r->doctoral_degree}}
-                                @else
-                                <h5 class="card-title">{{ $r->{'fname_'.app()->getLocale()} }} {{ $r->{'lname_'.app()->getLocale()} }}</h5>
-                                @endif
-
-
-                                <!-- <h5 class="card-title">{{ $r->{'fname_'.app()->getLocale()} }} {{ $r->{'lname_'.app()->getLocale()} }}</h5> -->
-                                <h5 class="card-title-2">{{ $r->{'academic_ranks_'.app()->getLocale()} }}</h5>
-                                @else
-                                <h5 class="card-title">{{ $r->{'position_'.app()->getLocale()} }}
-                                    {{ $r->{'fname_'.app()->getLocale()} }} {{ $r->{'lname_'.app()->getLocale()} }}
-                                </h5>
-                                @endif
-                                <p class="card-text-1">{{ trans('message.expertise') }}</p>
-                                <div class="card-expertise">
-                                    @foreach($r->expertise->sortBy('expert_name') as $exper)
-                                    <p class="card-text"> {{$exper->expert_name}}</p>
-                                    @endforeach
-                                </div>
+                            <p class="card-text-1">{{ trans('books.expertise') }}</p>
+                            <div class="card-expertise">
+                                @foreach($r->expertise->sortBy('expert_name') as $exper)
+                                <p class="card-text"> {{$exper->expert_name}}</p>
+                                @endforeach
+                            </div>
                         </div>
-                    </diV>
+                    </div>
+
                 </div>
             </div>
         </a>
