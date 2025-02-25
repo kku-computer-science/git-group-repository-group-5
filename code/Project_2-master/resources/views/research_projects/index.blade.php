67f6856593feae5ee1dfa0cfc03b379d4638fae3
warinee341-6 <warinee.a@kkumail.com>
2025-02-26 03:38:56 +0700

    Update index.blade.php

--- code/Project_2-master/resources/views/research_projects/index.blade.php
+++ code/Project_2-master/resources/views/research_projects/index.blade.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.3/css/fixedHeader.bootstrap4.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.12.0/css/dataTables.bootstrap4.min.css">
 
-@section('title', translateText('Project'))
+@section('title','Project')
 
 @section('content')
 <div class="container">
@@ -29,7 +29,7 @@
                         <th>{{ translateText('Project name') }}</th>
                         <th>{{ trans('message.Head') }}</th>
                         <th>{{ translateText('Member') }}</th>
-                        <th width="auto">{{ translateText('Action') }}</th>
+                        <th>{{ translateText('Action') }}</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -47,9 +47,9 @@
                                     @foreach($researchProject->user as $user)
                                         @if ($user->pivot->role == 1)
                                             @if (app()->getLocale() == 'th')
-                                                {{ $user->fname_th }}
+                                            {{ $user->fname_th }}
                                             @else
-                                                {{ $user->fname_en }}
+                                            {{ $user->fname_en }}
                                             @endif
                                         @endif
                                     @endforeach
@@ -58,9 +58,9 @@
                                     @foreach($researchProject->user as $user)
                                         @if ($user->pivot->role == 2)
                                             @if (app()->getLocale() == 'th')
-                                                {{ $user->fname_th }}
+                                            {{ $user->fname_th }}
                                             @else
-                                                {{ $user->fname_en }}
+                                            {{ $user->fname_en }}
                                             @endif
                                         @endif
                                     @endforeach
@@ -108,17 +108,72 @@
 
 <!-- Include jQuery and DataTables Scripts -->
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js" defer></script>
+<script src="http://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js" defer></script>
 <script src="https://cdn.datatables.net/1.12.0/js/dataTables.bootstrap4.min.js" defer></script>
 <script src="https://cdn.datatables.net/fixedheader/3.2.3/js/dataTables.fixedHeader.min.js" defer></script>
 
 <script>
     $(document).ready(function() {
+        // ดึง locale จาก Blade
+        let locale = "{{ app()->getLocale() }}";
+
+        let languageSettings = {};
+
+        if (locale === 'en') {
+            languageSettings = {
+                lengthMenu: "Show _MENU_ entries",
+                zeroRecords: "No matching records found",
+                info: "Showing _START_ to _END_ of _TOTAL_ entries",
+                infoEmpty: "No records available",
+                infoFiltered: "(filtered from _MAX_ total records)",
+                search: "Search:",
+                paginate: {
+                    first: "First",
+                    last: "Last",
+                    next: "Next",
+                    previous: "Previous"
+                }
+            };
+        } else if (locale === 'zh') {
+            languageSettings = {
+                lengthMenu: "显示 _MENU_ 条目",
+                zeroRecords: "未找到匹配的记录",
+                info: "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+                infoEmpty: "没有可用记录",
+                infoFiltered: "(从 _MAX_ 条记录中过滤)",
+                search: "搜索:",
+                paginate: {
+                    first: "首页",
+                    last: "末页",
+                    next: "下页",
+                    previous: "上页"
+                }
+            };
+        } else {
+            // สมมติว่าถ้าเป็น 'th' หรือภาษาอื่น ใช้ภาษาไทย
+            languageSettings = {
+                lengthMenu: "แสดง _MENU_ รายการ",
+                zeroRecords: "ไม่พบข้อมูลที่ตรงกัน",
+                info: "แสดง _START_ ถึง _END_ จากทั้งหมด _TOTAL_ รายการ",
+                infoEmpty: "ไม่มีข้อมูล",
+                infoFiltered: "(กรองจากทั้งหมด _MAX_ รายการ)",
+                search: "ค้นหา:",
+                paginate: {
+                    first: "หน้าแรก",
+                    last: "หน้าสุดท้าย",
+                    next: "ถัดไป",
+                    previous: "ก่อนหน้า"
+                }
+            };
+        }
+
         var table1 = $('#example1').DataTable({
             responsive: true,
-            fixedHeader: true,
+            language: languageSettings
         });
     });
+
+
 </script>
 
 <script type="text/javascript">
@@ -148,5 +203,4 @@
             });
     });
 </script>
-
 @stop
